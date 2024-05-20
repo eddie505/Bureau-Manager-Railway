a6f65007ead8e98a6f394dfcd61c4f15b3c58070
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-19 23:52:57 -0600

    Stream

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,8 @@
 module.exports = function override(config, env) {
     config.resolve.fallback = {
       "os": require.resolve("os-browserify/browser"),
-      "crypto": require.resolve("crypto-browserify")
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify")
     };
     return config;
   }

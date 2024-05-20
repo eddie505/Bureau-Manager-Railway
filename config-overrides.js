684ecae706d2c97184f15551f617600a818e293d
eddie <124751851+eddie505@users.noreply.github.com>
2024-05-19 23:56:57 -0600

    VM BROW

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,8 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
       "os": require.resolve("os-browserify/browser"),
       "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify")
+      "stream": require.resolve("stream-browserify"),
+      "vm": require.resolve("vm-browserify")
     };
     return config;
   }
